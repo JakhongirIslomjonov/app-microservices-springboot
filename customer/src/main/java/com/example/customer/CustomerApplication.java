9109dda62caa73b17c23e93cd16cfd69964f4df0
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-07-06 20:50:20 +0500

    microservice  the cleaned needn't path url

--- customer/src/main/java/com/example/customer/CustomerApplication.java
+++ customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -2,10 +2,7 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 @EnableFeignClients(basePackages = "com.example.clients")
 @SpringBootApplication
 public class CustomerApplication {
