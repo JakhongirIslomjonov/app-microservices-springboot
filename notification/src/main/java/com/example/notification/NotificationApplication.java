c9a6a9354cdc7ae666e068cb931c2cf02f3740dc
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-07-09 01:49:42 +0500

    rabbitmq added

--- notification/src/main/java/com/example/notification/NotificationApplication.java
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -1,12 +1,20 @@
 package com.example.notification;
 
+import com.example.amqp.RabbitMQMessageProducer;
+import lombok.Data;
 import org.springframework.amqp.core.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.example.notification","com.example.amqp"
+        }
+)
 public class NotificationApplication {
 
     public static void main(String[] args) {
@@ -31,10 +39,11 @@ public class NotificationApplication {
                 .with("secret-key");
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(AmqpTemplate amqpTemplate){
+/*    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer){
         return args -> {
-            amqpTemplate.convertAndSend("myinternalexchange","secret-key","hello every one");
+            producer.publish(new Data("esmat",12),"myinternalexchange","secret-key");
         };
-    }
+    }*/
+//    record Data(String name , Integer age){}
 }
