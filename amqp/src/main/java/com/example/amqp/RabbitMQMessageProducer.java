d2f79d65b46a388a7b2b4870d98f7a38b47cdd70
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-07-09 01:26:17 +0500

    rabbitmq added

---
+++ amqp/src/main/java/com/example/amqp/RabbitMQMessageProducer.java
@@ -0,0 +1,19 @@
+package com.example.amqp;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+@AllArgsConstructor
+public class RabbitMQMessageProducer {
+    private final AmqpTemplate amqpTemplate;
+
+    public void publish(Object payload , String exchange , String routingKey){
+
+        log.info("Publishing  to{} using routing {} , Payload :{}",exchange, routingKey,payload);
+        amqpTemplate.convertAndSend(exchange, routingKey, payload);
+        log.info("Published to {}  using  routingKey {} .Payload: {}", exchange, routingKey,payload);
+    }
+}
--- notification/src/main/java/com/example/notification/NotificationApplication.java
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -1,7 +1,10 @@
 package com.example.notification;
 
+import org.springframework.amqp.core.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NotificationApplication {
@@ -10,4 +13,28 @@ public class NotificationApplication {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange("myinternalexchange");
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue("myqueue");
+    }
+
+    @Bean
+    public Binding notificationBinding(){
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with("secret-key");
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(AmqpTemplate amqpTemplate){
+        return args -> {
+            amqpTemplate.convertAndSend("myinternalexchange","secret-key","hello every one");
+        };
+    }
 }
